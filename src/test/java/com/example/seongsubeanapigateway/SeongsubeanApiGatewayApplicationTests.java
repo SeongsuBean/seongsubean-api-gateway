fdc235d16aabf86679a0aa01ed527a0b86e807d4
Ui-Geol <gamoon777@naver.com>
2025-07-14 08:08:57 +0900

    "first commit"

---
+++ src/test/java/com/example/seongsubeanapigateway/SeongsubeanApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.seongsubeanapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeongsubeanApiGatewayApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
